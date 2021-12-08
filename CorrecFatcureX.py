bc81a20f20dcb3676b8d3f694672b7bc13379a93
ArnaudGardille <90635018+ArnaudGardille@users.noreply.github.com>
2021-12-08 14:24:37 +0100

    Add files via upload

---
+++ CorrecFatcureX.py
@@ -0,0 +1,130 @@
+
+from facturx import *
+import pathlib
+import os
+from os.path import isfile, join
+
+current_directory = str(pathlib.Path(__file__).parent.resolve())
+rep = current_directory + '/anciens_fichiers_pdf/'
+
+
+
+def extractXML(pdf_filename, out_xml_filename='exctracted.xml', disable_xsd_check=False):
+
+    if not isfile(pdf_filename):
+        raise Exception('Argument %s is not a filename', pdf_filename)
+
+    pdf_file = open(pdf_filename, 'rb')
+    check_xsd = True
+    if disable_xsd_check:
+        check_xsd = False
+    # The important line of code is below !
+    
+    (xml_filename, xml_string) = get_xml_from_pdf(pdf_file, check_xsd=check_xsd)
+    #(xml_filename, xml_string) = get_facturx_xml_from_pdf(pdf_file, check_xsd=check_xsd)
+    
+    
+    if xml_filename and xml_string:
+        if isfile(out_xml_filename):
+            print('File %s already exists. Overwriting it!', out_xml_filename)
+        xml_file = open(out_xml_filename, 'wb')
+        xml_file.write(xml_string)
+        xml_file.close()
+        print('File %s generated', out_xml_filename)
+    else:
+        raise Exception('File %s has not been created', out_xml_filename)
+
+def checkXML(xml_file):
+
+
+    if not isfile(xml_file):
+        raise Exception('%s is not a filename', xml_file)
+    xml_file = open(xml_file, 'rb')
+    # The important line of code is below !
+
+    xml_check_xsd(xml_file, flavor='autodetect', level='autodetect')
+
+def createPDF(pdf_filename, xml_filename, output_pdf_filename, overwrite=True, disable_xsd_check=False, additional_attachment_filenames=[]):
+
+    for filename in [pdf_filename, xml_filename] + additional_attachment_filenames:
+        if not isfile(filename):
+            raise Exception('Argument %s is not a filename', filename)
+
+    xml_file = open(xml_filename, 'rb')
+    check_xsd = True
+    if disable_xsd_check:
+        check_xsd = False
+    pdf_metadata = None
+
+    if isfile(output_pdf_filename):
+        if overwrite:
+            print('File %s already exists. Overwriting it.',
+                output_pdf_filename)
+        else:
+            print('File %s already exists. Exit.', output_pdf_filename)
+
+    # The important line of code is below !
+    generate_from_file(
+        pdf_filename, xml_file, check_xsd=check_xsd,
+        pdf_metadata=pdf_metadata, output_pdf_file=output_pdf_filename)
+
+
+def correctXML(xml_filename):
+    with open(xml_filename, 'r') as bdata:
+        data = bdata.read()
+        
+        data = data.replace('\n', '').replace('\r', '').replace('\t', '')
+        
+
+        parts = data.split('ram:ID schemeID=')
+        l = len(parts)
+        [id_prec, num_prec] = ["", ""]
+        reste_prec = []
+        
+        for i in range(1, l-1):
+            [id, num] = parts[i].split('>')[:2]
+            reste = parts[i].split('>')[2:]
+    
+            if id == id_prec:
+                L = [id_prec, num_prec[:9]+num_prec[14:]]
+                L.extend(reste)
+                parts[i] = ">".join(L)
+
+            [id_prec, num_prec] = [id, num]
+            reste_prec = reste
+        
+        
+        newData = 'ram:ID schemeID='.join(parts)
+        
+
+        path_xml = current_directory + '/corrected.xml'
+        
+        f = open(path_xml,"w",encoding="utf-8")
+        f.write(newData)
+        f.close()
+        
+
+try:
+    fichiers = [f for f in os.listdir(rep) if isfile(join(rep, f))]
+except:
+    os.mkdir(rep)
+    fichiers = [f for f in os.listdir(rep) if isfile(join(rep, f))]
+
+if not os.path.isdir(current_directory + '/nouveaux_fichiers_pdf/'):
+    os.mkdir(current_directory + '/nouveaux_fichiers_pdf/')
+
+
+
+print('fichiers: ', fichiers)
+for fichier in fichiers:
+    print(fichier)
+    if fichier[-3:] != "pdf":
+        continue
+    path = current_directory + '/anciens_fichiers_pdf/' + fichier
+    extractXML(path)
+    checkXML('exctracted.xml')
+    correctXML('exctracted.xml')
+    path_new_pdf = current_directory + '/nouveaux_fichiers_pdf/' + fichier 
+
+    createPDF(path, 'corrected.xml', path_new_pdf)
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Correction_XMLs
-Des outils permettant de corriger les fichiers XML des PDFs au format Factur-X
+# Correct the xml in your pdf files
+
+Start by installing the requirements:
+```
+pip3 install -r requirements
+```
+Then install your pdf inside the folder 'anciens_fichiers_pdf', and run 
+
+```
+python3 correction_XML.py
+```
+And get your corrected files in 'nouveaux_fichiers_pdf' 
+
+Enjoy!
